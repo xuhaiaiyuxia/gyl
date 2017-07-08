f01077eeab607ab9b9eee2ad842018ac70357ff2
xuhaiaiyuxia <18530971373@163.com>
2017-07-08 09:47:39 +0800

    hahahha

--- src/main/java/cn/project/gyl/App.java
+++ src/main/java/cn/project/gyl/App.java
@@ -4,7 +4,11 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.PropertySource;
 import org.springframework.context.annotation.PropertySources;
-
+/**
+ * 
+ * @author Administrator
+ *
+ */
 @SpringBootApplication
 @PropertySources({
 	@PropertySource("classpath:custom/custom.properties")
